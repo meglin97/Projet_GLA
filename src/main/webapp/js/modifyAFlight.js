6319dc750ed5364aa9d3ef26815d4d73dafe2935
DESKTOP-8FGHSN7\Shuvo Das <shuvo.eidd@gmail.com>
2021-05-04 20:59:39 +0200

    modify a Flight

--- src/main/webapp/js/modifyAFlight.js
+++ src/main/webapp/js/modifyAFlight.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#plan_flight_form').on('submit', function(event){
+    $('#plan_flight_form').on('submit', async function(event){
 		event.preventDefault();
 
 		const departure_date = document.getElementById("departure_date").value;
@@ -8,23 +8,29 @@ $(function () {
 		const arrival_time = document.getElementById("arrival_time").value;
 
         const current_user = JSON.parse(sessionStorage.getItem("current_user"));
-        console.log(current_user);
+        const flightID = document.getElementById("flight_id").value;
+		const flight = await getFlightInfo(flightID);
+
+		if (!flight || flight.pilot != current_user.userID){
+			$.notify("Cannot modify: The flight does not exist or you are not the pilot of this flight", { className: "error"});
+			return null;
+		}
 		
 		let data = {
+			flightID: flightID,
             departureAirfield: document.getElementById("departure_airfield").value,
             arrivalAirfield: document.getElementById("arrival_airfield").value,
             departureDate: departure_date + "T" + departure_time + ":00.000+02:00",
             arrivalDate: arrival_date + "T" + arrival_time + ":00.000+02:00",
             numberPlaces: document.getElementById("nb_places").value,
-            pilot: current_user.userID,
             ticketPrice: document.getElementById("ticket_price").value
 		};
 
 		$.ajax({
-			type: 'PUT',
+			type: 'POST',
 			contentType: "application/json",
 			data: JSON.stringify(data),
-			url: "/ws/coavionnage/flights/add"
+			url: "/ws/coavionnage/flights/edit"
 		}).done((response)=>{
 			console.log(response);
 			window.location.href = "/home.html";
@@ -53,3 +59,22 @@ $('#arrival_time').datetimepicker({
 	format:'H:i',
 	datepicker:false
 });
+
+async function getFlightInfo(flightID) {
+    let result;
+
+    try {
+        result = await $.ajax({
+            url: "ws/coavionnage/flights/" + flightID,
+            type: 'GET',
+        }).done((response)=>{
+			// console.log(response);
+		}).catch((error)=>{
+			console.error(error);
+		});
+
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
